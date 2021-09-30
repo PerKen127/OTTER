0b798228775565cc66ed1887339bb7f42e499c8a
Josh <jkeny127@gmail.com>
2021-09-30 17:41:17 -0400

    THE REAL Week 3 Final but with my name this time

--- projects/Week3-Starter/src/main.cpp
+++ projects/Week3-Starter/src/main.cpp
@@ -56,7 +56,7 @@ GLFWwindow* window;
 // The current size of our window in pixels
 glm::ivec2 windowSize = glm::ivec2(800, 800);
 // The title of our GLFW window
-std::string windowTitle = "100787173";
+std::string windowTitle = "Joshua Kenyon - 100787173";
 
 void GlfwWindowResizedCallback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
